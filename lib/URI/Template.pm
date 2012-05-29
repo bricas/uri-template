9dfe9d070f220f826048c8e1e6cffc2d061fdddb
Brian Cassidy <bricas@cpan.org>
2012-05-29 16:19:43 -0300

    fix year

--- lib/URI/Template.pm
+++ lib/URI/Template.pm
@@ -387,7 +387,7 @@ URI object.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007-20012 by Brian Cassidy
+Copyright 2007-2012 by Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
